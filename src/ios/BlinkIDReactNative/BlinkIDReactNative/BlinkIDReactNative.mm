fff727575d9f995cb9bb2315ecd39620be174d8a
Mark Vayngrib <mark@tradle.io>
2018-04-12 11:29:18 -0400

    feat: add 'timeout' option

--- src/ios/BlinkIDReactNative/BlinkIDReactNative/BlinkIDReactNative.mm
+++ src/ios/BlinkIDReactNative/BlinkIDReactNative/BlinkIDReactNative.mm
@@ -56,6 +56,7 @@ static NSString* const kOptionUseFrontCameraJsKey = @"useFrontCamera";
 static NSString* const kOptionReturnDocumentImageJsKey = @"shouldReturnDocumentImage";
 static NSString* const kOptionShouldReturnSuccessfulImageJsKey = @"shouldReturnSuccessfulImage";
 static NSString* const kOptionReturnFaceImageJsKey = @"shouldReturnFaceImage";
+static NSString* const kOptionTimeout = @"timeout";
 static NSString* const kRecognizersArrayJsKey = @"recognizers";
 
 // js keys for recognizer types
@@ -246,8 +247,13 @@ RCT_REMAP_METHOD(scan, scan:(NSString *)key withOptions:(NSDictionary*)scanOptio
 
     self.scannedImageSuccesful = nil;
 
-    // Do not timeout
-    settings.scanSettings.partialRecognitionTimeout = 0.0f;
+    NSTimeInterval timeout = [[self.options valueForKey:kOptionTimeout] doubleValue];
+    if (timeout) {
+        settings.scanSettings.partialRecognitionTimeout = timeout;
+    } else {
+        // Do not timeout
+        settings.scanSettings.partialRecognitionTimeout = 0.0f;
+    }
 
     /** 2. Setup the license key */
 
