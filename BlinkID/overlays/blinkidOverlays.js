e62cee0152ed22de7910ddbfec605aa38cdaaf86
Mark Vayngrib <mark@tradle.io>
2018-10-03 10:46:29 -0400

    feat: support custom tooltip texts

--- BlinkID/overlays/blinkidOverlays.js
+++ BlinkID/overlays/blinkidOverlays.js
@@ -5,8 +5,10 @@ import { OverlaySettings } from '../overlaySettings'
  * Document overlay is best suited for recognizers that perform ID document scanning.
  */
 export class DocumentOverlaySettings extends OverlaySettings {
-    constructor() {
+    constructor(opts={}) {
         super('DocumentOverlaySettings');
+        /** tootlip text that is defined under document view finder. */
+        this.tooltipText = opts.tooltipText;;
     }
 }
 
@@ -15,39 +17,51 @@ export class DocumentOverlaySettings extends OverlaySettings {
  * Document verification overlay is best suited for combined recognizers - recognizer that perform scanning of both sides of ID documents.
  */
 export class DocumentVerificationOverlaySettings extends OverlaySettings {
-    constructor() {
+    constructor(opts={}) {
         super('DocumentVerificationOverlaySettings');
-        /** 
-         * String: splash message that is shown before scanning the first side of the document, while starting camera.
-         * If null, default value will be used.
-         */
-        this.firstSideSplashMessage = null;
-        /** 
-         * String: splash message that is shown before scanning the second side of the document, while starting camera.
-         * If null, default value will be used.
-         */
-        this.secondSideSplashMessage = null;
-        /** 
-        * String: splash message that is shown after scanning the document.
-        * If null, default value will be used.
-        */
-        this.scanningDoneSplashMessage = null;
-        /** 
-         * String: user instructions that are shown above camera preview while the first side of the
-         * document is being scanned.
-         * If null, default value will be used.
-         */
-        this.firstSideInstructions = null;
-        /** 
-         * String: user instructions that are shown above camera preview while the second side of the
-         * document is being scanned.
-         * If null, default value will be used.
-         */
-        this.secondSideInstructions = null;
         /**
-         * String: glare message that is shown if glare was detected while scanning document.
-         * If null, default value will be used.
+         * Returns/sets user instructions that are shown above camera preview while the first side of the
+         * document is being scanned.
+         *
+         * Default: string defined by "photopay_front_verification_document"
+         * key in strings file in Microblink.bundle
          */
-        this.glareMessage = null;
+        this.firstSideInstructions = opts.firstSideInstructions;
+
+        /**
+         * Returns/sets user instructions that are shown above camera preview while the second side of the
+         * document is being scanned.
+         *
+         * Default: string defined by "photopay_back_verification_document"
+         * key in strings file in Microblink.bundle
+         */
+        this.secondSideInstructions = opts.secondSideInstructions;
+
+        /**
+         * Returns/sets splash message that is shown before scanning the first side of the document,
+         * while starting camera.
+         *
+         * Default: string defined by "photopay_front_splash_verification_document"
+         * key in strings file in Microblink.bundle
+         */
+        this.firstSideSplashMessage = opts.firstSideSplashMessage;
+
+        /**
+         * Returns/sets splash message that is shown before scanning the second side of the document,
+         * while starting camera.
+         *
+         * Default: string defined by "photopay_back_splash_verification_document"
+         * key in strings file in Microblink.bundle
+         */
+        this.secondSideSplashMessage = opts.secondSideSplashMessage;
+
+        /**
+         * Returns/sets glare status message that is shown if glare detection is turned on
+         * and it is shown if glare is detected.
+         *
+         * Default: string defined by "photopay_glare_status"
+         * key in strings file in Microblink.bundle
+         */
+        this.glareStatusMessage = opts.glareStatusMessage;
     }
 }
